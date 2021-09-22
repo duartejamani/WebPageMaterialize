37a47e8814d152af3393ee82f0a08f8163cb08e5
duartejamani <duarte.jamani@gmail.com>
2021-09-22 01:04:25 -0500

    Este es el primer commit de la pagina en producción

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    // $('.carousel.carousel-slider').carousel({fullWidth: true});
+    // $('.carousel.carousel-slider').carousel({
+    //   fullWidth: true,
+    //   indicators: true
+    // });
+
+    $(document).ready(function(){
+      $('.slider').slider({
+        height: 500
+      });
+    });
+        
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
